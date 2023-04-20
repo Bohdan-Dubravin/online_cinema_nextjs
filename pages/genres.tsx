ae5a882ccfc43bc339be5fdc3fc952d7e9dfb64c
Bohdan-Dubravin <dubravin111@gmail.com>
2023-04-20 18:19:54 +0300

    add collections of genres

---
+++ pages/genres.tsx
@@ -0,0 +1,31 @@
+import { GetStaticProps, NextPage } from 'next'
+
+import Collections from '@/components/screens/collections/Collections'
+import { ICollection } from '@/components/screens/collections/collections.types'
+
+import { GenreService } from '@/services/genre/genre.service'
+
+const GenresPage: NextPage<{ collections: ICollection[] }> = ({
+	collections,
+}) => {
+	return <Collections collections={collections || []} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	try {
+		const { data: collections } = await GenreService.getCollections()
+
+		return {
+			props: { collections },
+		}
+	} catch (e) {
+		// console.log(errorCatch(e))
+
+		return {
+			props: {},
+			// notFound: true,
+		}
+	}
+}
+
+export default GenresPage
